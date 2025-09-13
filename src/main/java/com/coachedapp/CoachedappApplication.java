f4396bf80609b33481ee0a1d97e6233f4bdd6103
EminDri <dariemin@outlook.com>
2025-09-13 19:14:24 +0100

    Create Spring Boot Controller, Service and Repository classes.
    Create the file to establish the connection with the Docker container.
    
    Create main class which holds the fields for the feedbacks.
    
    Add basic get and post methods to the controller class with the implementations called from the service class.

---
+++ src/main/java/com/coachedapp/CoachedappApplication.java
@@ -0,0 +1,13 @@
+package com.security.coachedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachedappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachedappApplication.class, args);
+    }
+
+}
